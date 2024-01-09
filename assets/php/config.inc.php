90e63b4984d1d2c53de38a44e5d4b142d40e617d
drew-chase <dcmanproductions@gmail.com>
2024-01-09 14:07:10 -0500

    Update database password in config file

--- assets/php/config.inc.php
+++ assets/php/config.inc.php
@@ -2,6 +2,6 @@
 // Database configuration
 $DB_HOST = "127.0.0.1";
 $DB_USER = "root";
-$DB_PASSWORD = "";
+$DB_PASSWORD = "9bc9f6b264724051";
 $DB_NAME = "pricing";
 $_ENV["HASH_SALT"] = "f0806be9-5b19-4f52-b66c-f8646adadf37";
