69cc47f0118e4604f73b0e75683c50592fca7788
drew-chase <dcmanproductions@gmail.com>
2024-01-10 16:29:46 -0500

    Update DB_USER in config.inc.php

--- assets/php/config.inc.php
+++ assets/php/config.inc.php
@@ -1,7 +1,7 @@
 <?php
 // Database configuration
 $DB_HOST = "127.0.0.1";
-$DB_USER = "root";
+$DB_USER = "drew";
 $DB_PASSWORD = "9bc9f6b264724051";
 $DB_NAME = "pricing";
 $_ENV["HASH_SALT"] = "f0806be9-5b19-4f52-b66c-f8646adadf37";
