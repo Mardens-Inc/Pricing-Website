2c0d8379d628c16f53a89b177cb47ff6eafa0b09
drew-chase <dcmanproductions@gmail.com>
2024-01-11 10:03:40 -0500

    Add popups section to PHP code

---
+++ assets/php/sections/popups.php
@@ -0,0 +1,11 @@
+<!-- POPUPS ZONE -->
+<?php
+require_once "edit-pricing-options.php";
+require_once "edit-list-item.php";
+require_once "list-item-options.php";
+require_once "voice-search.php";
+require_once "loading.php";
+require_once "create-pricing-entry.php";
+require_once "create-database-entry.php";
+require_once "admin-actions.php";
+require_once "admin-login.php";
