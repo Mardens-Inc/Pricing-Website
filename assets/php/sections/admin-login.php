106251565a65965bb1ceb086b262f20bee8090f6
drew-chase <dcmanproductions@gmail.com>
2024-01-11 10:03:11 -0500

    Add admin login popup

---
+++ assets/php/sections/admin-login.php
@@ -0,0 +1,21 @@
+<div id="admin-login-popup" class="popup">
+    <div class="popup-content">
+        <div class="popup-header row center vertical">
+            <div class="title fill">Admin Login</div>
+            <button class="close">x</button>
+        </div>
+
+        <div class="form-input col center horizontal">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" placeholder="Username" autocomplete="username">
+        </div>
+        <div class="form-input col center horizontal">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="Password" autocomplete="current-password">
+        </div>
+        <p id="login-error" class="error center horizontal"></p>
+        <div class="row center horizontal">
+            <button class="login">Login</button>
+        </div>
+    </div>
+</div>
