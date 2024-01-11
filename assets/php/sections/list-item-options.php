a5f046f53f03b3648d657209c807036962bd5c13
drew-chase <dcmanproductions@gmail.com>
2024-01-11 10:03:27 -0500

    Add list item options popup

---
+++ assets/php/sections/list-item-options.php
@@ -0,0 +1,31 @@
+<div class="popup" id="list-item-options-popup">
+    <div class="popup-content center vertical col">
+        <div class="popup-header row center vertical">
+            <div class="title fill">Actions</div>
+            <button class="close">x</button>
+        </div>
+
+        <table id="admin-action">
+            <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr id="print-button" onclick="print()">
+                    <td>Print Entry</td>
+                    <td><button>Action</button></td>
+                </tr>
+                <tr id="edit-entry-button" onclick="openEditListItem();">
+                    <td>Edit Entry</td>
+                    <td><button>Action</button></td>
+                </tr>
+                <tr id="delete-entry-button">
+                    <td>Delete Entry</td>
+                    <td><button>Action</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
