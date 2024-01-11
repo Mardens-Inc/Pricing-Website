2564e2827a730e41ac343011cd41a2e481c7e5f5
drew-chase <dcmanproductions@gmail.com>
2024-01-11 10:03:31 -0500

    Add loading popup section

---
+++ assets/php/sections/loading.php
@@ -0,0 +1,7 @@
+<div class="popup no-close" id="loading-popup">
+    <div class="col center horizontal vertical" style="color: white; height: 100%; transform: translateY(-200px)">
+        <h1 style="font-size: 6rem; margin-bottom: 0;">Loading</h1>
+        <h4 style="font-size: 2rem; margin-top: 0;">Please Wait</h4>
+    </div>
+    <div class="loading"></div>
+</div>
