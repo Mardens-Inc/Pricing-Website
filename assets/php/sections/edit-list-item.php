997a16467a35b72c126fb4894eddda08906c1e6d
drew-chase <dcmanproductions@gmail.com>
2024-01-11 10:03:19 -0500

    Add edit-list-item.php section

---
+++ assets/php/sections/edit-list-item.php
@@ -0,0 +1,14 @@
+<div class="popup" id="edit-list-item-popup">
+    <div class="popup-content center vertical col">
+        <div class="popup-header row center vertical">
+            <div class="title fill">Edit Item</div>
+            <button class="close">x</button>
+        </div>
+
+        <div class="form-input">
+            <label for="">UPC</label>
+            <input type="text" name="upc" id="upc" placeholder="UPC" autocomplete="off">
+        </div>
+
+    </div>
+</div>
