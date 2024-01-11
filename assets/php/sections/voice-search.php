fe414adea86939f65d55a4e7bf6f492f152a9eba
drew-chase <dcmanproductions@gmail.com>
2024-01-11 10:03:36 -0500

    Add voice search popup section

---
+++ assets/php/sections/voice-search.php
@@ -0,0 +1,17 @@
+<div class="popup" id="voice-search-popup">
+    <div class="voice-notification">
+        <i class="fa-solid fa-microphone"></i>
+        <p>Listening...</p>
+        <div class="search-result">
+            <p id="voice-showing-message"></p>
+            <table class="search-table">
+                <thead>
+                    <tr>
+                    </tr>
+                </thead>
+                <tbody id="voice-table-body">
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
