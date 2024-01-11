13b8e5249ca851555f3c7841f6fde04752450aa5
drew-chase <dcmanproductions@gmail.com>
2024-01-11 11:38:27 -0500

    Refactor admin login form

--- assets/php/sections/admin-login.php
+++ assets/php/sections/admin-login.php
@@ -4,18 +4,20 @@
             <div class="title fill">Admin Login</div>
             <button class="close">x</button>
         </div>
+        <div class="popup-body">
 
-        <div class="form-input col center horizontal">
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username" placeholder="Username" autocomplete="username">
-        </div>
-        <div class="form-input col center horizontal">
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" placeholder="Password" autocomplete="current-password">
-        </div>
-        <p id="login-error" class="error center horizontal"></p>
-        <div class="row center horizontal">
-            <button class="login">Login</button>
+            <div class="form-input col center horizontal">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" placeholder="Username" autocomplete="username">
+            </div>
+            <div class="form-input col center horizontal">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="Password" autocomplete="current-password">
+            </div>
+            <p id="login-error" class="error center horizontal"></p>
+            <div class="row center horizontal">
+                <button class="login">Login</button>
+            </div>
         </div>
     </div>
 </div>
