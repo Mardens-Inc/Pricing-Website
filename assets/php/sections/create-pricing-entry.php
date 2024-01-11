de4483c425a8b2c97bc527daf4a50fdb95c0b802
drew-chase <dcmanproductions@gmail.com>
2024-01-11 10:03:15 -0500

    Add create-pricing-entry.php section

---
+++ assets/php/sections/create-pricing-entry.php
@@ -0,0 +1,15 @@
+<div class="popup" id="create-pricing-entry-popup">
+    <div class="popup-content center vertical col">
+        <div class="popup-header row center vertical">
+            <div class="title fill">Add Price(s)</div>
+            <button class="close">x</button>
+        </div>
+        <form class="form-input">
+            <div class="drag-drop-area" id="upload-pricing-data" accept="*.xlsx, *.csv"> </div>
+            <div id="comparison-form" class="col fill">
+            </div>
+
+            <button id="upload-pricing-data-button">Upload</button>
+        </form>
+    </div>
+</div>
