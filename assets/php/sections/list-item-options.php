d1e5bb0146ca4b463b7e39cc1286ca2f2f6d43da
drew-chase <dcmanproductions@gmail.com>
2024-01-11 11:38:44 -0500

    Refactor list-item-options.php to use a div for the popup body

--- assets/php/sections/list-item-options.php
+++ assets/php/sections/list-item-options.php
@@ -4,28 +4,25 @@
             <div class="title fill">Actions</div>
             <button class="close">x</button>
         </div>
-
-        <table id="admin-action">
-            <thead>
-                <tr>
-                    <th>Description</th>
-                    <!-- <th>Actions</th> -->
-                </tr>
-            </thead>
-            <tbody>
-                <tr id="print-button" onclick="print()">
-                    <td>Print Entry</td>
-                    <!-- <td><button>Action</button></td> -->
-                </tr>
-                <tr id="edit-entry-button" onclick="openEditListItem();">
-                    <td>Edit Entry</td>
-                    <!-- <td><button>Action</button></td> -->
-                </tr>
-                <tr id="delete-entry-button">
-                    <td>Delete Entry</td>
-                    <!-- <td><button>Action</button></td> -->
-                </tr>
-            </tbody>
-        </table>
+        <div class="popup-body">
+            <table id="admin-action">
+                <thead>
+                    <tr>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr id="print-button" onclick="print()">
+                        <td>Print Entry</td>
+                    </tr>
+                    <tr id="edit-entry-button" onclick="openEditListItem();">
+                        <td>Edit Entry</td>
+                    </tr>
+                    <tr id="delete-entry-button">
+                        <td>Delete Entry</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
