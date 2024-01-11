a854d95b4275fa798ed5510fb55d93b99288c088
drew-chase <dcmanproductions@gmail.com>
2024-01-11 11:38:23 -0500

    Refactor admin-actions.php layout

--- assets/php/sections/admin-actions.php
+++ assets/php/sections/admin-actions.php
@@ -4,39 +4,41 @@
             <div class="title fill">Admin Actions</div>
             <button class="close">x</button>
         </div>
-        <table id="admin-action">
-            <thead>
-                <tr>
-                    <th>Description</th>
-                    <!-- <th>Actions</th> -->
-                </tr>
-            </thead>
-            <tbody>
-                <tr onclick="openCreateDatabaseEntryPopup()">
-                    <td>Create a new Database Entry</td>
-                    <!-- <td><button>Action</button></td> -->
-                </tr>
-                <tr class="only-on-list-view" onclick="showAddPricingListItem()">
-                    <td>Add Price(s)</td>
-                    <!-- <td><button>Action</button></td> -->
-                </tr>
-                <tr class="only-on-list-view" onclick="showEditPricingListOptions()">
-                    <td>Edit Options</td>
-                    <!-- <td><button>Action</button></td> -->
-                </tr>
-                <tr class="only-on-list-view" onclick="database.deleteList()">
-                    <td>Delete Pricing List</td>
-                    <!-- <td><button>Action</button></td> -->
-                </tr>
-                <tr class="only-on-list-view" onclick="database.deleteAllRows()">
-                    <td>Delete All Rows</td>
-                    <!-- <td><button>Action</button></td> -->
-                </tr>
-                <tr class="logout">
-                    <td>Logout</td>
-                    <!-- <td><button>Action</button></td> -->
-                </tr>
-            </tbody>
-        </table>
+        <div class="popup-body">
+            <table id="admin-action">
+                <thead>
+                    <tr>
+                        <th>Description</th>
+                        <!-- <th>Actions</th> -->
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr onclick="openCreateDatabaseEntryPopup()">
+                        <td>Create a new Database Entry</td>
+                        <!-- <td><button>Action</button></td> -->
+                    </tr>
+                    <tr class="only-on-list-view" onclick="showAddPricingListItem()">
+                        <td>Add Price(s)</td>
+                        <!-- <td><button>Action</button></td> -->
+                    </tr>
+                    <tr class="only-on-list-view" onclick="showEditPricingListOptions()">
+                        <td>Edit Options</td>
+                        <!-- <td><button>Action</button></td> -->
+                    </tr>
+                    <tr class="only-on-list-view" onclick="database.deleteList()">
+                        <td>Delete Pricing List</td>
+                        <!-- <td><button>Action</button></td> -->
+                    </tr>
+                    <tr class="only-on-list-view" onclick="database.deleteAllRows()">
+                        <td>Delete All Rows</td>
+                        <!-- <td><button>Action</button></td> -->
+                    </tr>
+                    <tr class="logout">
+                        <td>Logout</td>
+                        <!-- <td><button>Action</button></td> -->
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
