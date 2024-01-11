054eb9e80c0169f1399a3a431ee79637865dd8b8
drew-chase <dcmanproductions@gmail.com>
2024-01-11 11:17:41 -0500

    Add styling and additional fields to edit pricing options form

--- assets/php/sections/edit-pricing-options.php
+++ assets/php/sections/edit-pricing-options.php
@@ -1,3 +1,11 @@
+<style>
+    .extra-section {
+        margin-bottom: 1rem;
+        background-color: rgba(0, 0, 0, .08);
+        padding: 1rem;
+    }
+</style>
+
 <div class="popup" id="edit-pricing-options-popup">
     <div class="popup-content center vertical col">
         <div class="popup-header row center vertical">
@@ -14,24 +22,35 @@
             <label for="po">PO#</label>
             <input type="text" name="po" placeholder="PO#" tabindex="3" required>
             <toggle name="print" value="true">Can print label?</toggle>
-            <div class="print-form fill" style="display:none">
-                <div class="col fill" style="margin-right: 5px">
-                    <label for="print-label">Print Label</label>
-                    <input type="text" name="print-label" placeholder="Print Label" tabindex="4">
+            <div class="print-form fill extra-section col" style="display:none">
+                <div class="row fill">
+                    <div class="col fill" style="margin-right: 5px">
+                        <label for="print-label">Print Label</label>
+                        <input type="text" name="print-label" placeholder="Print Label" tabindex="4">
+                    </div>
+                    <div class="col fill" style="margin-left: 5px">
+                        <label for="print-year">Print Year</label>
+                        <input type="text" name="print-year" placeholder="Print Year" tabindex="5">
+                    </div>
                 </div>
-                <div class="col fill" style="margin-left: 5px">
-                    <label for="print-year">Print Year</label>
-                    <input type="text" name="print-year" placeholder="Print Year" tabindex="5">
+                <div class="row fill">
+                    <div class="col fill" style="margin-left: 5px">
+                        <label for="print-price-column">Print Price Column</label>
+                        <select name="print-price-column" id="print-price-column">
+                        </select>
+                    </div>
+                    <div class="col fill" style="margin-left: 5px">
+                        <label for="print-retail-price-column">Print Retail Price Column</label>
+                        <select name="print-retail-price-column" id="print-retail-price-column">
+                        </select>
+                    </div>
                 </div>
-                <div class="col fill" style="margin-left: 5px">
-                    <label for="print-price-column">Print Price Column</label>
-                    <select name="print-price-column" id="print-price-column">
-                    </select>
-                </div>
+                <toggle name="print-show-retail" value="true">Show Retail Price?</toggle>
             </div>
+            <toggle name="show-date" value="false">Show Date Column?</toggle>
             <toggle name="voice-search" value="false">Enable voice searching?</toggle>
 
-            <div class="voice-form fill" style="display:none;margin-bottom: 1rem;">
+            <div class="voice-form fill extra-section" style="display:none;">
                 <div class="col fill" style="margin-right: 5px">
                     <label for="voice-description-column">Title/Description Column</label>
                     <select name="voice-description-column" id="voice-description-column">
