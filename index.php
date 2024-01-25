38fee2b070ecc95e30be026c14442dcc33aeb7ec
drew-chase <dcmanproductions@gmail.com>
2024-01-25 16:11:04 -0500

    Implement file download in index.php
    Introduced code in index.php to download a file if it does not exist or if a new version is available on the provided URL. Changes also include cleanup of the existing code formatting for better readability.

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<?php
+
+$url = "https://auth.mardens.com";
+$file = $_SERVER['DOCUMENT_ROOT'] . "/assets/lib/mardens-auth-lib.js";
+// Get last modified time of the url
+// Ignore ssl errors
+$last_modified = file_get_contents($url . "/?time", false, stream_context_create([
+    'http' => [
+        'method' => 'GET',
+        'header' => 'Content-Type: text/plaintext'
+    ], 'ssl' => [
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+    ],
+]));
+$last_modified = intval($last_modified);
+
+if (!file_exists($file) || $last_modified >= filemtime($file)) {
+    // Download the file
+    file_put_contents($file, file_get_contents($url . "/", false, stream_context_create([
+        'http' => [
+            'method' => 'GET',
+            'header' => 'Content-Type: text/javascript'
+        ], 'ssl' => [
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ],
+    ])));
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -36,46 +68,46 @@
 
 <body>
 
-    <nav>
-        <button id="admin-login" class="secondary">Administrator</button>
-        <button id="directory-button" class="only-on-list-view">Database Directory</button>
-    </nav>
-    <div id="content" class="col center horizontal fill">
-        <div class="row">
-            <img id="logo-image" src="/assets/images/logo.svg" alt="">
-            <div id="page-header">Pricing Database Directory</div>
+<nav>
+    <button id="admin-login" class="secondary">Administrator</button>
+    <button id="directory-button" class="only-on-list-view">Database Directory</button>
+</nav>
+<div id="content" class="col center horizontal fill">
+    <div class="row">
+        <img id="logo-image" src="/assets/images/logo.svg" alt="">
+        <div id="page-header">Pricing Database Directory</div>
+    </div>
+    <hr>
+    <div class="row">
+        <div id="count-area" class="row fill center vertical">
+            <span>Show </span>
+            <select name="entries" id="entries">
+                <option value="10" selected>10</option>
+                <option value="25">25</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+            </select>
+            <span> entries</span>
         </div>
-        <hr>
-        <div class="row">
-            <div id="count-area" class="row fill center vertical">
-                <span>Show </span>
-                <select name="entries" id="entries">
-                    <option value="10" selected>10</option>
-                    <option value="25">25</option>
-                    <option value="50">50</option>
-                    <option value="100">100</option>
-                </select>
-                <span> entries</span>
-            </div>
-            <div id="search-area" class="row fill">
-                <input type="search" name="search" id="search" placeholder="Type to filter results" title="search input">
-                <button id="clear-search" title="Clear search">x</button>
-            </div>
+        <div id="search-area" class="row fill">
+            <input type="search" name="search" id="search" placeholder="Type to filter results" title="search input">
+            <button id="clear-search" title="Clear search">x</button>
         </div>
-
-        <div class="row">
-            <table class="search-table"> </table>
-        </div>
-        <span id="showing-message" class="fill"></span>
-        <div class="pagination row center horizontal vertical fill"></div>
     </div>
 
-    <?php require_once "assets/php/sections/popups.php";    ?>
+    <div class="row">
+        <table class="search-table"></table>
+    </div>
+    <span id="showing-message" class="fill"></span>
+    <div class="pagination row center horizontal vertical fill"></div>
+</div>
 
-    <script src="/assets/js/authorization.min.js"></script>
-    <script src="/assets/js/directory.min.js"></script>
-    <script src="/assets/js/inputs.min.js"></script>
-    <script src="/assets/js/voice-search.min.js"></script>
+<?php require_once "assets/php/sections/popups.php"; ?>
+
+<script type="module" src="/assets/js/authorization.js"></script>
+<script src="/assets/js/directory.js"></script>
+<script src="/assets/js/inputs.js"></script>
+<script src="/assets/js/voice-search.js"></script>
 </body>
 
 </html>
