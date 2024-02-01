1211266339f8351676d90e5205e6eae2eff17b33
drew-chase <dcmanproductions@gmail.com>
2024-02-01 14:00:47 -0500

    Add comments and require statements to popups.php
    The code now includes additional comments for better clarity, along with corresponding require statements in the popups.php file. This ensures the necessary files are included for proper functionality, further structuring the code and enhancing maintainability.

--- assets/php/sections/popups.php
+++ assets/php/sections/popups.php
@@ -1,11 +1,20 @@
 <!-- POPUPS ZONE -->
 <?php
+echo "\n\n<!-- Edit Pricing Options -->\n\n";
 require_once "edit-pricing-options.php";
+echo "\n\n<!-- Edit List Item -->\n\n";
 require_once "edit-list-item.php";
+echo "\n\n<!-- List Item Options -->\n\n";
 require_once "list-item-options.php";
+echo "\n\n<!-- Voice Search -->\n\n";
 require_once "voice-search.php";
+echo "\n\n<!-- Loading -->\n\n";
 require_once "loading.php";
+echo "\n\n<!-- Create Pricing Entry Popup -->\n\n";
 require_once "create-pricing-entry.php";
+echo "\n\n<!-- Create database entry popup -->\n\n";
 require_once "create-database-entry.php";
+echo "\n\n<!-- Admin Actions Popup -->\n\n";
 require_once "admin-actions.php";
+echo "\n\n<!-- Admin Login Popup -->\n\n";
 require_once "admin-login.php";
