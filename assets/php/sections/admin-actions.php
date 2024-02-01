322add605d355fc040e3f5f2b0dbcdac0aa9a416
drew-chase <dcmanproductions@gmail.com>
2024-02-01 14:00:25 -0500

    Simplify admin actions HTML structure
    HTML structure in the admin actions section has been simplified by removing commented out table columns. This simplifies the code and improves readability, while keeping the functionality intact.

--- assets/php/sections/admin-actions.php
+++ assets/php/sections/admin-actions.php
@@ -7,36 +7,29 @@
         <div class="popup-body">
             <table id="admin-action">
                 <thead>
-                    <tr>
-                        <th>Description</th>
-                        <!-- <th>Actions</th> -->
-                    </tr>
+                <tr>
+                    <th>Description</th>
+                </tr>
                 </thead>
                 <tbody>
-                    <tr onclick="openCreateDatabaseEntryPopup()">
-                        <td>Create a new Database Entry</td>
-                        <!-- <td><button>Action</button></td> -->
-                    </tr>
-                    <tr class="only-on-list-view" onclick="showAddPricingListItem()">
-                        <td>Add Price(s)</td>
-                        <!-- <td><button>Action</button></td> -->
-                    </tr>
-                    <tr class="only-on-list-view" onclick="showEditPricingListOptions()">
-                        <td>Edit Options</td>
-                        <!-- <td><button>Action</button></td> -->
-                    </tr>
-                    <tr class="only-on-list-view" onclick="database.deleteList()">
-                        <td>Delete Pricing List</td>
-                        <!-- <td><button>Action</button></td> -->
-                    </tr>
-                    <tr class="only-on-list-view" onclick="database.deleteAllRows()">
-                        <td>Delete All Rows</td>
-                        <!-- <td><button>Action</button></td> -->
-                    </tr>
-                    <tr class="logout">
-                        <td>Logout</td>
-                        <!-- <td><button>Action</button></td> -->
-                    </tr>
+                <tr onclick="openCreateDatabaseEntryPopup()">
+                    <td>Create a new Database Entry</td>
+                </tr>
+                <tr class="only-on-list-view" onclick="showAddPricingListItem()">
+                    <td>Add Price(s)</td>
+                </tr>
+                <tr class="only-on-list-view" onclick="showEditPricingListOptions()">
+                    <td>Edit Options</td>
+                </tr>
+                <tr class="only-on-list-view" onclick="database.deleteList()">
+                    <td>Delete Pricing List</td>
+                </tr>
+                <tr class="only-on-list-view" onclick="database.deleteAllRows()">
+                    <td>Delete All Rows</td>
+                </tr>
+                <tr class="logout">
+                    <td>Logout</td>
+                </tr>
                 </tbody>
             </table>
         </div>
