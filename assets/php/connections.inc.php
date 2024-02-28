99bf39282978da09bb63ac00a8be4d04ad7f1f59
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-02-28 11:51:27 -0500

    Update allowed HTTP methods
    The list of allowed HTTP methods in the CORS headers set in the connections.inc.php file has been expanded. The previously allowed methods were GET, POST, DELETE, and OPTIONS. With this change, PATCH and PUT have now been included.

--- assets/php/connections.inc.php
+++ assets/php/connections.inc.php
@@ -2,7 +2,7 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
 
 /**
  * Database connection class
